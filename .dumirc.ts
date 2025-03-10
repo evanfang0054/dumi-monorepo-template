0aee4875964b13b1f303a429fb1ca976470cb9fc
evanfang0054 <380616679@qq.com>
2025-03-10 14:49:53 +0800

    chore: Configure base and public path for production deployment

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'dumi-monorepo-template';
+
 export default defineConfig({
   resolve: {
     // atomDirs: [{ type: 'component', dir: 'components' }], // 预留后续新增组件库时需要
@@ -19,4 +21,6 @@ export default defineConfig({
     prefersColor: { default: 'auto' },
     footer: `Copyright © ${new Date().getFullYear()} EvanFang`
   },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
